834d3cf1dfe8659dfaff1be8adc43624b8decdeb
Sulochana Kodituwakku <sulochana.456@live.com>
2016-11-14 13:28:18 +0530

    ready to push

---
+++ administrator/cache/com_virtuemart_cats/35da52a5c5f7457e395f015de79b78c4-cache-com_virtuemart_cats-be9f8d947d4a80a170ded6cb6db3bad9.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";s:182:"<option   value="4">Audio Device</option><option   value="3">Blackbox Servers</option><option selected=\"selected\"  value="1">Gaming PCs</option><option   value="2">Laptops</option>";}
---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,18 @@
+#
+#<?php die("Forbidden."); ?>
+
+2016-11-09 12:59:37 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at /opt/lampp/htdocs/redline/libraries/tcpdf
+2016-11-10 13:05:35 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system '/opt/lampp/htdocs/vmfiles/'. <br>You can also use complex folder name as 'password' for example '/opt/lampp/htdocs/redline/administrator/components/com_virtuemart/7JHKUIpxkC18/'. Use this link to the <a href='http://localhost/redline/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-10 13:08:21 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:08:21 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:09:22 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:09:54 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:09:57 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:11:29 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:16:11 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:16:11 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:16:54 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-10 13:17:03 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-14 06:52:30 ERROR vmError: The path for invoices seems to miss the correct file permissions Safe Path /opt/lampp/htdocs/vmfiles/
+2016-11-14 06:52:30 ERROR vmError: COM_VIRTUEMART_CANNOT_STORE_CONFIG
+2016-11-14 06:52:30 ERROR vmError: COM_VIRTUEMART_CANNOT_STORE_CONFIG
